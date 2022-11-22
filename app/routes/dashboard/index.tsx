c26524cf8f6a44115e914acdd96eccb52fd9f632
Phat Truong <phat.truong@asnet.com.vn>
2022-11-22 12:04:33 +0700

    Features/initial remix

--- app/routes/dashboard/index.tsx
+++ app/routes/dashboard/index.tsx
@@ -6,19 +6,17 @@ import { getUserById } from "~/models/user.server";
 
 import { requireUserId } from "~/session.server";
 
-export const loader = async ({ request, params }: LoaderArgs) => {
-  const userId = await requireUserId(request);
-  const user = await getUserById(userId);
-  return json({ user });
-};
+// TODO: required login user
+// export const loader = async ({ request, params }: LoaderArgs) => {
+//   const userId = await requireUserId(request);
+//   const user = await getUserById(userId);
+//   return json({ user });
+// };
 
 const Dashboard = () => {
-  const data = useLoaderData<typeof loader>();
-
   return (
     <div>
       <h1>Dashboard</h1>
-      <code>{JSON.stringify(data).toString()}</code>
     </div>
   );
 };
