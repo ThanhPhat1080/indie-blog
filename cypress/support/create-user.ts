170e1313aa455e1afece21bdddf167a529b52bce
Phat Truong <phat.truong@dfinsolutions.com>
2022-12-03 11:52:42 +0700

    Fix type

--- cypress/support/create-user.ts
+++ cypress/support/create-user.ts
@@ -20,7 +20,13 @@ async function createAndLogin(email: string) {
     throw new Error("All test emails must end in @example.com");
   }
 
-  const user = await createUser(email, "myreallystrongpassword");
+  const user = await createUser({
+    name: "Your name",
+    bio: "Your bio",
+    twitter: "Your twitter",
+    email,
+    password: "Your strong password",
+  });
 
   const response = await createUserSession({
     request: new Request("test://test"),
