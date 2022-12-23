b96acc65249cbb868b8912f7decd54aea40721d8
Phat Truong <thanhphat.994@gmail.com>
2022-12-23 11:49:15 +0700

    Update style lazy img

--- app/components/CloudinaryImageLoader.tsx
+++ app/components/CloudinaryImageLoader.tsx
@@ -136,19 +136,47 @@ export const CloudinaryImageLoader = ({
   height = "",
   className,
 }: propsType) => {
+
   return (
-    <Image
-      alt={alt}
-      src={src}
-      loaderUrl={ROUTERS.LOADER_CLOUDINARY_IMAGE}
-      loader={cloudinaryLoader}
-      options={options}
-      responsive={responsive}
-      width={width}
-      height={height}
-      className={className}
-      style={{minHeight: '100%', minWidth: '100%'}}
-    />
+    <div
+      className="relative inline-flex"
+      style={{ backgroundColor: "#ddd", width: "100%", height: "100%" }}
+    >
+      <div className="absolute-center z-0">
+        <svg
+          className="h-8 w-8 animate-spin text-slate-600 "
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          />
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          />
+        </svg>
+      </div>
+      <Image
+        alt={alt}
+        src={src}
+        loaderUrl={ROUTERS.LOADER_CLOUDINARY_IMAGE}
+        loader={cloudinaryLoader}
+        options={options}
+        responsive={responsive}
+        width={width}
+        height={height}
+        className={className}
+        style={{ minHeight: "100%", minWidth: "100%", zIndex: 1 }}
+      />
+    </div>
   );
 };
 
