0e84c357638df8c52391b355adf7e382f250667a
Phat Truong <thanhphat.994@gmail.com>
2022-12-26 11:39:43 +0700

    Add GA with partytown

--- app/root.tsx
+++ app/root.tsx
@@ -57,16 +57,24 @@ export default function App() {
         <Meta />
         <Links />
         <Partytown debug={true} forward={["dataLayer.push"]} />
-          <script type="text/partytown"
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-ML97CTM');`
-            }}
-          ></script>
+
+        {/* Google tag (gtag.js) */}
+        <script
+          type="text/partytown"
+          src="https://www.googletagmanager.com/gtag/js?id=G-6JM5FWM0XG"
+        />
+        <script
+          type="text/partytown"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6JM5FWM0XG');
+            `,
+          }}
+        />
+
       </head>
       <body className="h-full bg-white dark:bg-slate-800">
         <noscript>
